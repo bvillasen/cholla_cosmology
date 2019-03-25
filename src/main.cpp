5d6ef7485ee831705bf5498cfbb95606fbafe76b
bruno <bvillasen@gmail.com>
2019-03-25 10:54:34 -0700

    limit to 10 timesteps

--- src/main.cpp
+++ src/main.cpp
@@ -148,7 +148,7 @@ int main(int argc, char *argv[])
   // Evolve the grid, one timestep at a time
   chprintf("Starting calculations.\n");
   while (G.H.t < P.tout)
-  //while (G.H.n_step < 1)
+  while (G.H.n_step < 10)
   {
     chprintf("n_step: %d \n", G.H.n_step + 1 );
     // get the start time
