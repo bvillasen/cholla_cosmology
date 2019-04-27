d9d4bd699fc24b81ef27f8c7464b7026881db525
bruno <bvillasen@gmail.com>
2019-04-26 17:47:09 -0700

    writing extra_scalars

--- src/io.cpp
+++ src/io.cpp
@@ -1257,39 +1257,64 @@ void Grid3D::Write_Grid_HDF5(hid_t file_id)
     #endif //OUTPUT_TEMPERATURE
 
     #ifdef EXTRA_SCALAR
-    // Copy the internal energy array to the memory buffer
-    for (k=0; k<H.nz_real; k++) {
-      for (j=0; j<H.ny_real; j++) {
-        for (i=0; i<H.nx_real; i++) {
-          id = (i+H.n_ghost) + (j+H.n_ghost)*H.nx + (k+H.n_ghost)*H.nx*H.ny;
-          buf_id = k + j*H.nz_real + i*H.nz_real*H.ny_real;
-          dataset_buffer[buf_id] = C.extra_scalar[id];
+    // // Copy the internal energy array to the memory buffer
+    // for (k=0; k<H.nz_real; k++) {
+    //   for (j=0; j<H.ny_real; j++) {
+    //     for (i=0; i<H.nx_real; i++) {
+    //       id = (i+H.n_ghost) + (j+H.n_ghost)*H.nx + (k+H.n_ghost)*H.nx*H.ny;
+    //       buf_id = k + j*H.nz_real + i*H.nz_real*H.ny_real;
+    //       dataset_buffer[buf_id] = C.extra_scalar[id];
+    //     }
+    //   }
+    // }
+    // // Create a dataset id for density
+    // dataset_id = H5Dcreate(file_id, "/extra_scalar", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    // // Write the density array to file  // NOTE: NEED TO FIX FOR FLOAT REAL!!!
+    // status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dataset_buffer);
+    // // Free the dataset id
+    // status = H5Dclose(dataset_id);
+    // 
+    // // Copy the internal energy array to the memory buffer
+    // for (k=0; k<H.nz_real; k++) {
+    //   for (j=0; j<H.ny_real; j++) {
+    //     for (i=0; i<H.nx_real; i++) {
+    //       id = (i+H.n_ghost) + (j+H.n_ghost)*H.nx + (k+H.n_ghost)*H.nx*H.ny;
+    //       buf_id = k + j*H.nz_real + i*H.nz_real*H.ny_real;
+    //       dataset_buffer[buf_id] = C.extra_scalar[H.n_cells + id];
+    //     }
+    //   }
+    // }
+    // // Create a dataset id for density
+    // dataset_id = H5Dcreate(file_id, "/extra_scalar_1", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    // // Write the density array to file  // NOTE: NEED TO FIX FOR FLOAT REAL!!!
+    // status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dataset_buffer);
+    // // Free the dataset id
+    // status = H5Dclose(dataset_id);
+
+    for (int s=0; s<N_EXTRA_SCALARS; s++) {
+      // create the name of the dataset
+      char dataset[100]; 
+      char number[10];
+      strcpy(dataset, "/extra_scalar"); 
+      sprintf(number, "%d", s);
+      strcat(dataset,number);        
+      // Copy the scalar array to the memory buffer
+      for (k=0; k<H.nz_real; k++) {
+        for (j=0; j<H.ny_real; j++) {
+          for (i=0; i<H.nx_real; i++) {
+            id = (i+H.n_ghost) + (j+H.n_ghost)*H.nx + (k+H.n_ghost)*H.nx*H.ny;
+            buf_id = k + j*H.nz_real + i*H.nz_real*H.ny_real;
+            dataset_buffer[buf_id] = C.extra_scalar[id + s*H.n_cells];
+          }
         }
       }
+      // Create a dataset id for the scalar
+      dataset_id = H5Dcreate(file_id, dataset, H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+      // Write the scalar array to file  // NOTE: NEED TO FIX FOR FLOAT REAL!!!
+      status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dataset_buffer); 
+      // Free the dataset id
+      status = H5Dclose(dataset_id);
     }
-    // Create a dataset id for density
-    dataset_id = H5Dcreate(file_id, "/extra_scalar", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-    // Write the density array to file  // NOTE: NEED TO FIX FOR FLOAT REAL!!!
-    status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dataset_buffer);
-    // Free the dataset id
-    status = H5Dclose(dataset_id);
-    
-    // Copy the internal energy array to the memory buffer
-    for (k=0; k<H.nz_real; k++) {
-      for (j=0; j<H.ny_real; j++) {
-        for (i=0; i<H.nx_real; i++) {
-          id = (i+H.n_ghost) + (j+H.n_ghost)*H.nx + (k+H.n_ghost)*H.nx*H.ny;
-          buf_id = k + j*H.nz_real + i*H.nz_real*H.ny_real;
-          dataset_buffer[buf_id] = C.extra_scalar[H.n_cells + id];
-        }
-      }
-    }
-    // Create a dataset id for density
-    dataset_id = H5Dcreate(file_id, "/extra_scalar_1", H5T_IEEE_F64BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-    // Write the density array to file  // NOTE: NEED TO FIX FOR FLOAT REAL!!!
-    status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, dataset_buffer);
-    // Free the dataset id
-    status = H5Dclose(dataset_id);
     #endif//EXTRA_SCALAR
 
 
